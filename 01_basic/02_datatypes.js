b0e012c92cfe07ca694d5fed1d86bf8cedb3971c
pratikgondkar <pratikgondkar4040@gmail.com>
2024-04-25 18:40:33 +0530

    dataTypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3 ) //code readbility should be high
+
+console.log("Pratik")
+
+/* dataTypes */
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
