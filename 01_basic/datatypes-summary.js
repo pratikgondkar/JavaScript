d1ef352ce76f3c3c30a87effed5d190c6a2d56a6
pratikgondkar <pratikgondkar4040@gmail.com>
2024-04-26 00:17:12 +0530

    datatypes-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,34 @@
+/* Primittive */
+
+// 7 types => String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId); //false
+
+const BigNumber = 3258755984
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Pratik",
+    age: 22
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof BigNumber);
+
+
+// Reference (Non Primittive)
+
+// Array, Object, Functions
