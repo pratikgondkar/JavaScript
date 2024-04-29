bfb956ab699ee1bf94f7134b7d31a4b26abb4ff1
pratikgondkar <pratikgondkar4040@gmail.com>
2024-04-30 00:05:00 +0530

    Control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+/* if */
+/* condition => <, >, <=, >=, ==, !=, ===  */
+
+const isUserLoggedIn = true
+// if(2 != 3) {
+//     console.log("excuted");
+// }
+const temperature = 41
+
+// if ( temperature < 50 ) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greature than 50");
+// }
+// console.log("Execute");
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power ${power}`);
+// }
+// console.log(`User Power ${power}`);
+
+// const balance = 1000
+// const balance = 700
+
+// if (balance > 500) console.log("text"), console.log("text2"); /* as kadhi lihu naha ekach line mandhe donh lihu nako */
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggesdInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to Buy Corse");
+}
+
+if (loggedInFromGoogle || loggesdInFromEmail) {
+    console.log("user is login in ");
+}
+    
